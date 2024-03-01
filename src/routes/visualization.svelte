6ca830a4001090fafed9f21ada4ce2c7efbc244e
Yiheng Yuan <yiy159@ucsd.edu>
2024-02-29 17:26:33 -0800

    Delete visualization.svelte

--- src/routes/visualization.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import Introduction from '../components/Introduction.svelte';
-    import WageDisparityChart from '../components/WageDisparityChart.svelte';
-</script>
-
-<Introduction/>
-<WageDisparityChart/>
-<!-- Further visualization content can follow here -->
